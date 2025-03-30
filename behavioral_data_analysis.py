eb988cde501c1ee860e1f29b24b5c97794306681
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2025-03-30 17:20:53 +0200

    Create behavioral_data_analysis.py

---
+++ behavioral_data_analysis.py
@@ -0,0 +1,18 @@
+#%%
+
+## Script for Girls' Day 3rd April, 2025
+
+### Data: in folder "output" in cwd (current working directory 
+
+### JGU Jupyter Webserver: https://cbdm-01.zdv.uni-mainz.de/~muro/teaching/p4b/mod4-2/SoSe23/c0_set_up/c0_jgu_jupyter_notebook_server.html
+
+#%%
+# imports (pre-installed libraries)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pt
+import scipy
+import os
+import re
+
+#%%
